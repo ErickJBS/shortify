d7d8ab3d808c39723886b4cf303ba808ae80d706
ErickJBS <a311008@uach.mx>
2020-03-11 21:17:09 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+const app = new express();
+
+app.listen(port, () => {
+    console.log('App started on port', port);
+});
